dbde24496e95e061d2b6392023c954be38e6d351
khbyun <novemberde@nate.com>
2018-01-01 21:14:54 +0900

    delete console.log

--- app/handler.js
+++ app/handler.js
@@ -7,7 +7,7 @@ const SLACK_URL = require('./slack.config.json').URL;
 module.exports.crawler = (event, context, callback) => {
   let result;
 
-  got('https://www.naver.com').then(res => {
+  return got('https://www.naver.com').then(res => {
     const $ = cheerio.load(res.body);
 
     result = $('.ca_item .ca_a');
@@ -26,7 +26,6 @@ module.exports.crawler = (event, context, callback) => {
       }),
     };
 
-    console.log(result.text());
     callback(null, response);
   });
 
